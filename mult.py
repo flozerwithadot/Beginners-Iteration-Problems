807733cab5e5b4279b9f0c659b4bbdfd5f34e2fc
flozer :3 <161006821+flozerwithadot@users.noreply.github.com>
2024-04-03 07:47:25 +0000

    line 10 (unfinished)

---
+++ mult.py
@@ -0,0 +1,10 @@
+positive = False
+while not positive:
+    number = int(input("Enter a number: "))
+    if number > 0:
+        positive = True
+    else:
+        print("Not a positive integer.")
+name = input("Enter your name: ")
+for x in range(number):
+    print(x)
